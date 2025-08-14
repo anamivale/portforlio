bc12d1a9002f9e63f36d8bbe0a39e9a538755fa4
anamivale <muhembelev@gmail.com>
2025-08-14 16:20:33 +0300

    feat: replace placeholder text with logo image in Footer component

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,10 +1,11 @@
+import Image from "next/image";
 import React from "react";
 
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
-        <span>LOGO</span>
+            <Image className="border border-white rounded-full" width={50} height={50} src="/Valeria Muhembele Logo Design.png" alt="Vercel Logo" />
         <p className="text-slate-600">All rights reserved.</p>
       </div>
     </footer>
